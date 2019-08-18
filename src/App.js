ffff39481b62182d97d5acc7f49534380c579f8f
ashanperera201 <AshanPerera.201@gmail.com>
2019-08-18 17:14:13 +0530

    adding components and styles for those.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import HomePage from './page/homepage/home.component';
 import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact={true} path='/shop' component={ShopPage} />
+        <Route exact={true} path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );

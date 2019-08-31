f86791a63efc61202602aeb569bfd8ecb27e4096
ashanperera201 <AshanPerera.201@gmail.com>
2019-09-01 00:59:45 +0530

    adding collection item and shop component with sub routings.

--- src/page/shop/shop.component.jsx
+++ src/page/shop/shop.component.jsx
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../collection/collection.component';
 
-class ShopPage extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        return (
-            <div className='shop-page'>
-                {
-                    this.state.collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CategoryPage} />
+    </div>
+);
 
 export default ShopPage;
